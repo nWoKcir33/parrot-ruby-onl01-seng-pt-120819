6975f5b228cd26142bf7136dd7fa1b22f713fe36
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-11 13:21:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase = "Squawk")
-  puts "#{phrase}!"
-"#{phrase}!"
+def parrot(phrase = "Squawk!")
+  puts "#{phrase}"
+"#{phrase}"
 end
 
 parrot
